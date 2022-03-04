856a1f8349990fee654df16b84dc7e2f2f3d58d8
slehan <212836+slehan@users.noreply.github.com>
2022-03-04 21:13:06 +0000

    Deploying to gh-pages from @ slehan/xivbosses@c3940cd30986ac7b1ff6b409d06ddd54bfece9f3 🚀

--- component---src-pages-page-2-js-2dec472debc8ffa5f80f.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{9863:function(e,t,a){a.r(t);var l=a(7294),n=a(5444),r=a(2755),c=a(9470);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-2dec472debc8ffa5f80f.js.map
